ff6768116e83d437d3150159aae3e01fa8724c6d
Alex <outta057@gmail.com>
2026-06-20 17:10:12 +0300

    dobavil paginacii na transaction

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import type { Expense } from "../types/expense";
-
+import { useState } from "react";
 type Props = {
 	expenses: Expense[];
 };
 
 const TransactionList: React.FC<Props> = ({ expenses }) => {
+
+	const [currentPage, setCurrentPage] = useState(1);
+
+const itemsPerPage = 7;
+
+const totalPages = Math.ceil(expenses.length / itemsPerPage);
+
+const startIndex = (currentPage - 1) * itemsPerPage;
+const endIndex = startIndex + itemsPerPage;
+
+const currentExpenses = expenses.slice(startIndex, endIndex);
 
 
 
@@ -20,7 +31,7 @@ const TransactionList: React.FC<Props> = ({ expenses }) => {
 						добавьте расход.
 					</p>
 				) : (
-					expenses.map((expense, index) => (
+					currentExpenses.map((expense, index) => (
 						<div
 							key={index}
 							className="flex justify-between border-b border-gray-700 py-2"
@@ -41,6 +52,42 @@ const TransactionList: React.FC<Props> = ({ expenses }) => {
 						</div>
 					))
 				)}
+
+				{totalPages > 1 && (
+	<div className="flex justify-center gap-2 mt-4">
+		<button
+			onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+			disabled={currentPage === 1}
+			className="px-3 py-1 border rounded disabled:opacity-50"
+		>
+			←
+		</button>
+
+		{Array.from({ length: totalPages }, (_, i) => (
+			<button
+				key={i}
+				onClick={() => setCurrentPage(i + 1)}
+				className={`px-3 py-1 border rounded ${
+					currentPage === i + 1
+						? "bg-blue-500 text-white"
+						: "text-gray-300"
+				}`}
+			>
+				{i + 1}
+			</button>
+		))}
+
+		<button
+			onClick={() =>
+				setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+			}
+			disabled={currentPage === totalPages}
+			className="px-3 py-1 border rounded disabled:opacity-50"
+		>
+			→
+		</button>
+	</div>
+)}
 			</section>
 		</div>
 	);
